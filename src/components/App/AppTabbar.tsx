cd0697f1f58d6fb4b2147adf6606013792ebaf2b
Zeng Tao <qingchun1995@gmail.com>
2020-09-04 17:31:18 +0800

    Add Tabbar icons

--- src/components/App/AppTabbar.tsx
+++ src/components/App/AppTabbar.tsx
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { Tag, FileText, PieChart } from '@styled-icons/feather'
 
 const AppTabbarStyled = styled.nav`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   ul {
     display: flex;
   }
 
   li {
     flex: auto;
-    text-align: center;
   }
 
   a {
-    display: block;
-    padding: 16px;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    padding: 9px;
+
+    svg {
+      height: 20px;
+      width: 20px;
+    }
   }
 `
 
@@ -24,15 +33,24 @@ class AppTabbar extends Component {
       <AppTabbarStyled>
         <ul>
           <li>
-            <Link to="/tags">标签</Link>
+            <Link to="/tags">
+              <Tag />
+              <span>标签</span>
+            </Link>
           </li>
 
           <li>
-            <Link to="/details">明细</Link>
+            <Link to="/details">
+              <FileText />
+              <span>明细</span>
+            </Link>
           </li>
 
           <li>
-            <Link to="/statistics">统计</Link>
+            <Link to="/statistics">
+              <PieChart />
+              <span>统计</span>
+            </Link>
           </li>
         </ul>
       </AppTabbarStyled>
